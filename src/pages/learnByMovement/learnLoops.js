ebb686980cf3d1ce04c288061480962152c2db8b
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-07-23 12:02:09 +0100

    using getusermedia
    Co-authored-by: Michael  Devlin <michaeldevfay@gmail.com>

--- src/pages/learnByMovement/learnLoops.js
+++ src/pages/learnByMovement/learnLoops.js
@@ -6,14 +6,39 @@ export default function LearnLoops() {
   return (
     <div>
       <StartInstructions />
-      <div class="drum-container">
-        <Styledimg src={drum} alt="drum" />
-      </div>
+
+      <Drumcont>
+        <video autoplay></video>
+        <Styledimg class="virtual-drum" src={drum} alt="drum" />
+      </Drumcont>
     </div>
   );
 }
+/**************************************/
+var constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(function (mediaStream) {
+    var video = document.querySelector("video");
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function (e) {
+      video.play();
+    };
+  })
+  .catch(function (err) {
+    console.log(err.name + ": " + err.message);
+  });
+
+/************************************/
 
 const Styledimg = styled.img`
   width: 30%;
   height: 30%;
+`;
+const Drumcont = styled.div`
+  width: 50vw;
+  height: 60vh;
+  margin: 40px;
+  border: solid 5px black;
 `;
