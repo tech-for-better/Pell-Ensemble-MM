100ad5f135052daf3d69d49598c46bb01df7b6e5
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-07-23 11:17:51 +0100

    pic for camerafram
    Co-authored-by: Michael  Devlin <michaeldevfay@gmail.com>

--- src/pages/learnByMovement/learnLoops.js
+++ src/pages/learnByMovement/learnLoops.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import StartInstructions from '../../components/StartInstructions'
-
+import React from "react";
+import StartInstructions from "../../components/StartInstructions";
+import drum from "../../images/drum.png";
+import styled from "styled-components";
 export default function LearnLoops() {
-    return (
+  return (
     <div>
-        <StartInstructions />
+      <StartInstructions />
+      <div class="drum-container">
+        <Styledimg src={drum} alt="drum" />
+      </div>
     </div>
-    )
+  );
 }
+
+const Styledimg = styled.img`
+  width: 30%;
+  height: 30%;
+`;
