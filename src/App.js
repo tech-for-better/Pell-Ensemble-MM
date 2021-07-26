bb72b13abd8a4b0580735a7316c03069438001ab
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-07-26 10:20:03 +0100

    adding learnAlgorithm page to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import LearnSequencing from "./pages/learnByMovement/learnSequences";
 import LearnLoops from "./pages/learnByMovement/learnLoops";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} 
-from 'react-router-dom';
+import LearnAlgorithm from "./pages/learnByMovement/learnAlgorithm";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-          <ul>
-            <li>
-              <Link to="/learnSequencing">Learn Sequencing</Link>
-            </li>
-            <li>
-              <Link to="/learnLoops">Learn Loops</Link>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link to="/learnSequencing">Learn Sequencing</Link>
+          </li>
+          <li>
+            <Link to="/learnLoops">Learn Loops</Link>
+          </li>
+          <li>
+            <Link to="/learnAlgorithm">Learn Algorithm</Link>
+          </li>
+        </ul>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -30,6 +29,9 @@ function App() {
           </Route>
           <Route path="/learnLoops">
             <LearnLoops />
+          </Route>
+          <Route path="/learnAlgorithm">
+            <LearnAlgorithm />
           </Route>
         </Switch>
       </div>
