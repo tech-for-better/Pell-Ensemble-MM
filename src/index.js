d0afc608f2fad98be65aa664a0ca0bfc06831e8d
mikailbadoula <michaeldevfay@gmail.com>
2021-08-02 16:46:50 +0100

    import fonts and add styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyles from './styles/GlobalStyle.js';
+import Typography from './styles/Typography';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
+    <Typography />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
