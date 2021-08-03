c53b26bf994169800fc2ec5456078b4bbdc34492
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-08-03 10:33:06 +0100

    changing the counter and setcount in  callback

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ export default function Counter({ count }) {
   return (
     <CountDiv>
       <p>Counter:</p>
-      <p>{count}</p>
+      <CountPara>{count}</CountPara>
     </CountDiv>
   );
 }
@@ -16,4 +16,7 @@ const CountDiv = styled.div`
   background-color: #91bbba;
   text-align: center;
   padding-top: 1%;
+`;
+const CountPara = styled.p`
+  font-size: 48px;
 `;
